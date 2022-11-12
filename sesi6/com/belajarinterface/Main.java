4d7f4b337b9b19dd554b65524101adce13bb2925
Muhammad Syarif Hidayat <92723671+mshidayat66@users.noreply.github.com>
2022-11-12 14:27:42 +0700

    Tugas Sesi 6

---
+++ sesi6/com/belajarinterface/Main.java
@@ -0,0 +1,42 @@
+package com.sesi6.belajarinterface;
+
+import java.util.Scanner;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Gunakan Laptop");
+        Laptop thinkpad = new Lenovo();
+        LaptopUser andri = new LaptopUser(thinkpad);
+        
+        while(true){
+            System.out.print("Masukan input ON/OFF/UP/DOWN/OUT? ");
+            String input = in.next();
+            gunakanlaptop(andri, input);
+            if(input.equals("OUT")){
+                System.out.println("Keluar");
+                break;
+            }
+        }
+    }
+
+    public static void gunakanlaptop(LaptopUser user, String input) {
+        if(input.equals("ON")){
+            user.turnOnLaptop();
+        }
+        
+        else if(input.equals("OFF")){
+            user.turnOffLaptop();
+        }
+        
+        else if(input.equals("UP")){
+            user.makeLaptopLouder();
+        }
+        
+        else if(input.equals("DOWN")){
+            user.makeLaptopSilence();
+        }
+    }
+}
